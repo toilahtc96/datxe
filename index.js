19d38dd50b44611757e419a2cf30d36426210679
toilahtc96 <toilahtc@gmail.com>
2020-09-12 18:44:44 +0700

    xxx

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
+test = (async() => {
 
-async function main() {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox"]
-  });
-  const tab = await browser.newPage();
-  const text = await (await tab.goto("http://example.com/")).text();
-  console.log(text);
-  console.log("donea");
-  browser.close();
-}
+    let bookcarUrl = 'https://hasonhaivan.vn/admin_lv2/modules/account/login.php';
 
-main();
+
+    let browser = await puppeteer.launch({ headless: false });
+    // let browser = await puppeteer.launch({
+        // headless: false
+        // args: ['--no-sandbox', '--disable-setuid-sandbox']
+    //  });
+    let page = await browser.newPage();
+
+    await page.goto(bookcarUrl, { waitUntil: 'networkidle2' });
+
+    await page.evaluate(() => {
+        let username = document.querySelector('div[class="form-group has-feedback"] > input[name="username"]');
+        let password = document.querySelector('div[class="form-group has-feedback"] > input[name="password"]');
+        let submit = document.querySelector('button[type="submit"]');
+        username.value = "dl.hhtravelvn";
+        password.value = "Hiepnguyen12345689";
+
+        submit.click();
+
+
+
+
+
+    });
+
+    await page.waitForSelector(".treeview-menu");
+
+    await page.evaluate(() => {
+        let linkToBook = document.querySelector('ul[class="treeview-menu"] > li > a');
+        linkToBook.click();
+    });
+
+    await page.waitForSelector(".view_adm_search_element");
+    await page.select('.form-control.bv_form_tuyen', "6");
+    await page.waitFor(4000)
+
+    let dataCa = await page.evaluate(async() => {
+        let ca = document.querySelector('div[class="view_adm_search_element"] > select[class="form-control bvv_ca_select"]');
+        let ngay = document.querySelector('div[class="view_adm_search_element"] > input[class="form-control bv_form_day hasDatepicker"]');
+        ca.value = 1;
+        ngay.value = "12-09-2020";
+    });
+    var gioDi = await page.evaluate(async() => {
+
+        return $(`li.bv_top_not_li`)[0].innerHTML
+    });
+    console.log(gioDi)
+        // await browser.close();
+})();
