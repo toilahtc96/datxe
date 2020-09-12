3fdfd7452b5fae66134f4aee3648dde5407129c9
toilahtc96 <toilahtc@gmail.com>
2020-09-12 18:25:55 +0700

    xxx

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ async function main() {
   const tab = await browser.newPage();
   const text = await (await tab.goto("http://example.com/")).text();
   console.log(text);
-  console.log("done");
+  console.log("donea");
   browser.close();
 }
 
